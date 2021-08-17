65418a3bf48329ffeae449ebef94bc09359cb6c0
Yashar Zafari <yasharez@gmail.com>
2021-08-17 16:49:06 -0700

    Sorted out beginning setup

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,11 +2,20 @@ require_relative 'player'
 
 puts "Player 1 enter name:"
 player1_name = gets.chomp
-puts "Player 1 enter 'x' or 'o':"
-player1_symbol = gets.chomp
+player1_symbol = ''
+loop do
+  puts "Player 1 pick 'X' or 'O':"
+  player1_symbol = gets.chomp.upcase
+  break if player1_symbol == 'X' || player1_symbol == 'O'
+end
 
-player = Player.new(player1_name, player1_symbol)
+puts "Player 2 enter name:"
+player2_name = gets.chomp
+player2_symbol = case player1_symbol
+                 when 'X' then 'O'
+                 when 'O' then 'X'
+                 end
 
+player1 = Player.new(player1_name, player1_symbol)
+player2 = Player.new(player2_name, player2_symbol)
 
-puts player.name
-puts player.symbol
